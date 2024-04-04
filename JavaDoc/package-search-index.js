d3e56e835de2ec5c7e4ce959afdc62dd5762d264
r0bert-p <c3062623@newcastle.ac.uk>
2024-04-04 17:44:37 +0100

    Version 1.0 JavaDoc documentation commit

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"subscription"},{"l":"subscription.manager"},{"l":"subscription.manager.exceptions"}];updateSearchResults();
